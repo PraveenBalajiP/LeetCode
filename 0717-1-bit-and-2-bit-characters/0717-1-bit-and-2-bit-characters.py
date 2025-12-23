627562cbb6d8ff8986ee8caad5ea3830d5aa8a0b
PRAVEEN <praveenbalajipb14@gmail.com>
2025-12-23 18:59:53 +0530

    Time: 0 ms (100%), Space: 17.4 MB (99.74%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -4,10 +4,7 @@ class Solution:
         l=[]
         i=0
         while i<len(bits)-1:
-            if bits[i]==1 and bits[i+1]==0:
-                l.append(str(bits[i])+str(bits[i+1]))
-                i=i+1
-            elif bits[i]==1 and bits[i+1]==1:
+            if (bits[i]==1 and bits[i+1]==0) or (bits[i]==1 and bits[i+1]==1):
                 l.append(str(bits[i])+str(bits[i+1]))
                 i=i+1
             else:
